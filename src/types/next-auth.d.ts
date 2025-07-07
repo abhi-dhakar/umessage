fd1aaf3d313aed6cc35754790b452e0f5a920cab
abhi dhakar <nagarabhishek011@gmail.com>
2025-07-07 11:55:23 +0530

    fix: standardize provider name casing and clean up type definitions in next-auth

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,28 +1,28 @@
-import 'next-auth'
-import { DefaultSession } from 'next-auth';
+import 'next-auth';
 
 declare module 'next-auth' {
-    interface User{
-        _id?: string;
-        username?: string;
-        isVerified?: boolean;
-        isAcceptingMessage?: boolean;
-    }
-  interface Session{
-      user: {
-         _id?: string;
-         username?: string;
-         isVerified?: boolean;
-         isAcceptingMessage?: boolean;
-      } & DefaultSession['user']
-   }
+  interface Session {
+    user: {
+      _id?: string;
+      isVerified?: boolean;
+      isAcceptingMessage?: boolean;
+      username?: string;
+    } & DefaultSession['user'];
+  }
+
+  interface User {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessage?: boolean;
+    username?: string;
+  }
 }
 
 declare module 'next-auth/jwt' {
-    interface JWT {
-      _id?: string;
-      username?: string;
-      isVerified?: boolean;
-      isAcceptingMessage?: boolean;
-    }
+  interface JWT {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessage?: boolean;
+    username?: string;
+  }
 }
